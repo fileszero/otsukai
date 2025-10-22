323fa577108caaff1c6a32330d76b6a0350e7ce7
FILES=0 <files.eq.zero@gmail.com>
2025-10-22 13:03:31 +0900

    Update webapp

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3946252160"
+    serviceWorkerVersion: "3106546769"
   }
 });
